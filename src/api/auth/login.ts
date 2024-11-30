d674921729808d7fff47918e3a4a911160c75c99
Rahul Nair <rahulsunilnair1074@gmail.com>
2024-11-30 06:36:08 +0530

    fix:error message

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -3,14 +3,10 @@ import { loginSchema } from '@/types/authtypes';
 import client from '../client';
 
 export const login = async (credentials: loginSchema) => {
-	try {
-		const response = await client.post(
-			`${process.env.VITE_SUB_URL}/auth/login`,
-			credentials
-		);
+	const response = await client.post(
+		`${process.env.VITE_SUB_URL}/auth/login`,
+		credentials
+	);
 
-		return response.data;
-	} catch (error) {
-		console.error('login.ts', error);
-	}
+	return response.data;
 };
